e6e26a5758802cf11de15dbdd2b8f3a083a079ee
John Lian R. Nerecina <johnliannerecina@gmail.com>
2026-03-03 00:16:39 +0000

    Activity2 Uploaded Date: March 03, 2026 Programmer: John Lian Nerecina

---
+++ Activity2/index.php
@@ -0,0 +1,159 @@
+<html>
+    <head>
+        <title>CCC311-18 / CCL311-18 Applications Development & Emerging Technologies</title>
+    </head>
+    <body>
+        <h1>Activity 2</h1>
+            <link rel="stylesheet" href="css/bootstrap.min.css">
+            <script src="js/jquery.js"></script>
+            <script src = "js/bootstrap.min.js"></script>
+
+            <div class="container-fluid">
+                <a href="#">Messages<span class="badge">10</span></a>
+                <a href="#">Notification<span class="badge">30</span></a>
+                <button>Click to see notification<span class="badge">500</span></button>
+
+                <div class="page-header">
+                    <h3>Download <span class="label label-default">Version 10.1</span></h3>
+                </div>
+            </div>    
+            <br/>
+            <br/>
+            <hr/>
+
+            <div class="container-fluid">
+                <div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+                <div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+                <div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+            </div>
+
+            <br/>
+            <br/>
+            <hr/>
+
+            <div class="container-fluid">
+                <div class="jumbotron">
+                    <h1>Heading in jumbotron</h1>
+                </div>
+                    <h1>Heading without jumbotron</h1>
+
+                <div class="page-header">
+                    <h1>Welcome</h1>
+                </div>
+
+                <p class="well well-sm">This is the well</p>
+                <p class="well">This is the well</p>
+                <p class="well well-lg">This is the well</p>
+                
+            </div>
+
+            <br/>
+            <br/>
+            <hr/>
+
+            <div class="container-fluid">
+                <span class="glyphicon glyphicon-trash"></span>:Trash Can <br/>
+                <span class="glyphicon glyphicon-pencil"></span>:Pencil <br/>
+                <button class="btn btn-success"><span class="glyphicon glyphicon-search"></span> Find</button>
+            </div>
+
+            <br/>
+            <br/>
+            <hr/>
+
+
+            <div class="container-fluid">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">CCS</a></li>
+                        <li><a href="#">CBA</a></li>
+                        <li><a href="#">CEA</a></li>
+                        <li class="divider"></li>
+                        <li class="dropdown-header">Exit</li>
+                        <li><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <div>
+                    <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a href="#">CCS</a></li>
+                        <li><a href="#">CBA</a></li>
+                        <li><a href="#">CEA</a></li>
+                        <li class="divider"></li>
+                        <li class="dropdown-header">Exit</li>
+                        <li><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="dropup">
+                <div>
+                    <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">CCS</a></li>
+                        <li><a href="#">CBA</a></li>
+                        <li><a href="#">CEA</a></li>
+                        <li class="divider"></li>
+                        <li class="dropdown-header">Exit</li>
+                        <li><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <br/>
+            <br/>
+            <hr/>
+
+            <div class="container-fluid">
+                <button class="btn btn-default">Click Me</button>
+                <button class="btn btn-primary btn-xs">Click Me</button>
+                <button class="btn btn-info btn-lg">Click Me</button>
+                <button class="btn btn-success btn-md">Click Me</button>
+                <button class="btn btn-warning">Click Me</button>
+                <button class="btn btn-danger btn-sm">Click Me</button>
+                <button class="btn btn-link">Click Me</button>
+                <button class="btn btn-danger btn-block">Click Me</button>
+                <button class="btn btn-danger disabled">Click Me</button>
+                <button class="btn btn-danger active">Click Me</button>
+
+                <div class="btn-group">
+                        <button class="btn btn-info">Home</button>
+                        <button class="btn btn-info">About</button>
+                        <button class="btn btn-info">Contact</button>
+                        <button class="btn btn-info">Products</button>
+                </div>
+
+                <div class="btn-group-vertical">
+                        <button class="btn btn-info">Home</button>
+                        <button class="btn btn-info">About</button>
+                        <button class="btn btn-info">Contact</button>
+                        <button class="btn btn-info">Products</button>
+                </div>
+
+                <div class="btn-group btn-group-justified">
+                        <button class="btn btn-info">Home</button>
+                        <button class="btn btn-info">About</button>
+                        <button class="btn btn-info">Contact</button>
+                        <button class="btn btn-info">Products</button>
+                </div>
+
+            </div>
+
+
+            <hr/>
+            <br/>
+            <div class="container-fluid">
+                <img src="pic/MyProfile.jpg">
+                <img src="pic/MyProfile.jpg" class="img-rounded"> 
+                <img src="pic/MyProfile.jpg" class="img-circle"> 
+                <img src="pic/MyProfile.jpg" class="img-thumbnail">
+            </div>
+
+
+    </body>
+
+</html>
